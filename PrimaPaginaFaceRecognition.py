f8986763edf53276c8f35a3f8e8930d0ed2df562
Akasha <121229419+Maka-Melasha@users.noreply.github.com>
2024-02-29 20:43:56 +0200

    Init

---
+++ PrimaPaginaFaceRecognition.py
@@ -0,0 +1,27 @@
+
+import cv2
+import os 
+import random
+import numpy as np
+from matplotlib import pyplot as plt
+
+cap = cv2.VideoCapture(0)
+
+if not (cap.isOpened()):
+    print("Could not open video device")
+
+cap.set(3, 176)
+cap.set(4, 144)
+
+while(True):
+# Capture frame-by-frame
+    ret, frame = cap.read()
+# Display the resulting frame
+    cv2.imshow('preview',frame)
+# Waits for a user input to quit the application
+    if cv2.waitKey(1) & 0xFF == ord('q'):       
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
