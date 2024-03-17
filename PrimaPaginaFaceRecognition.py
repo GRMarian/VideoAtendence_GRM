e80ced14dcd9ab65857ee08bd77143909f8664ec
Akasha <ghenea.razvan@yahoo.com>
2024-03-17 11:05:46 +0200

    face crop
    am renuntat la histograme.
    face identifieryul da rateuri. imput prost
    face cropul este de 227x227 pentru reteaua preantrenata alexnet

--- PrimaPaginaFaceRecognition.py
+++ PrimaPaginaFaceRecognition.py
@@ -21,12 +21,7 @@ if not (cap.isOpened()):
 #cap.set(3, 176)
 #cap.set(4, 144)
     
-# Create a figure for the histogram
-fig, ax = plt.subplots()
-ax.set_title("Grayscale Histogram")
-ax.set_xlabel("grayscale value")
-ax.set_ylabel("pixel count")
-ax.set_xlim([0.0, 255.0]) # Adjusted to match grayscale range
+
 
 
 while True:
@@ -38,31 +33,35 @@ while True:
 
 
     frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    frame_gray_adjust = exposure.rescale_intensity(frame_gray, in_range=(170/255, 250/255), out_range=(0, 1))
+    #frame_gray_adjust = exposure.rescale_intensity(frame_gray, in_range=(170/255, 250/255), out_range=(0, 1))
     frame_gray_equ = cv2.equalizeHist(frame_gray)
-    hist = cv2.calcHist([frame_gray], [0], None, [256], [0, 256])
-    cv2.normalize(hist, hist)
-    ax.clear()
-    ax.plot(hist)
-    ax.set_xlim([0.0, 255.0])
 
     face_classifier = cv2.CascadeClassifier(
         cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
     )
     face = face_classifier.detectMultiScale(
-        frame_gray, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
+        frame_gray_equ, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
     )
     for (x, y, w, h) in face:
+        crop_face = frame[y:y+h,x:x+w]
+        crop_face = cv2.resize( crop_face, [227,227])
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4)
 
 
+    hist, bins = np.histogram(frame_gray,256,[0, 256])
+    hist2, bins = np.histogram(frame_gray_equ,256,[0, 256])
     cv2.imshow('Original', frame)
     cv2.imshow('Gray', frame_gray)
-    cv2.imshow('Gray', frame_gray_equ)
-    cv2.imshow('Adjust', frame_gray_adjust)
+    cv2.imshow('GrayEqu', frame_gray_equ)
+    #cv2.imshow('Adjust', frame_gray_adjust)
+    cv2.imshow('Crop', crop_face)
+    #plt.figure("gray")
+    #plt.hist(hist,bins=255,range=(0,255))
+    #plt.figure("gray equ")
+    #plt.hist(hist2,bins=255,range=(0,255))
 
     # Update the histogram plot
-    plt.pause(0.01) # Pause for a short time to update the plot
+    #plt.pause(0.5) # Pause for a short time to update the plot
 
     # Break the loop on 'q' key press
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -71,4 +70,4 @@ while True:
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
-plt.close(fig)
+plt.close()
